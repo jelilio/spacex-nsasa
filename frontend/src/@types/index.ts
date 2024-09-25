c4a7e23a490a9284faf9f1f953a109abd954950a
Jelili Adesina <jelilio.adesina@gmail.com>
2024-09-25 01:37:31 +0100

    provided pagination models

---
+++ frontend/src/@types/index.ts
@@ -0,0 +1,55 @@
+export type ImageLibObject = {
+  collection: ImageLibCollection;
+};
+
+export type ImageLibCollection = {
+  version: string;
+  href: string;
+  items: ImageItem[];
+  metadata: Metadata;
+};
+
+export type ImageItem = {
+  href: string;
+  data: ImageItemData[];
+  links: Link[];
+};
+
+export type ImageItemData = {
+  album: string[];
+  center: string;
+  title: string;
+  photographer: string;
+  keywords: string[];
+  location: string;
+  nasa_id: string;
+  media_type: string;
+  date_created: string;
+  description: string;
+};
+
+export type Link = {
+  href: string;
+  rel: string;
+  prompt: string;
+  notfound: string;
+};
+
+export interface Metadata {
+  total_hits: number;
+}
+
+export interface Paged<T> {
+  content: T[];
+  meta: MetaFields;
+  nextPage?: number;
+  previousPage?: number;
+}
+
+export interface MetaFields {
+  size: number;
+  page: number;
+  numberOfElements: number;
+  totalPages: number;
+  totalElements: number;
+}
